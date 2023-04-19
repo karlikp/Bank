dd50ea1ac5d3934d8b3859491f01552f0c9c97cd
karlikp <pitera.karol@gmail.com>
2023-04-19 19:46:08 +0200

    add class credit and main file

---
+++ Bank_cpp/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+	int credit_kind{};
+	cout << "This program is able to get a credit for you" << endl;
+	cout << "********************************************" << endl;
+
+	cout << "Would you like recive a credit for individual customers or a company credit? ";
+	cout << "To select an individual credit, enter: 1. Whereas to choose a company credit, enter: 2.";
+	cin >> credit_kind;
+
+
+}
